70aa272961547e8bd760b0eda005d93058e44612
fabuandras <fabuandras@gmail.com>
2025-12-03 15:51:00 +0100

    Asztal model

--- src/model/Asztal.java
+++ src/model/Asztal.java
@@ -1,5 +1,36 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Asztal {
-    
+    private String szin;
+    private List<Etel> etelek;
+
+    public Asztal(String szin) {
+        this.szin = szin;
+        this.etelek = new ArrayList<>();
+    }
+
+    public String getSzin() {
+        return szin;
+    }
+
+    public void addEtel(Etel e) {
+        if (etelek.size() < 5) {
+            etelek.add(e);
+        }
+    }
+
+    public List<Etel> getEtelek() {
+        return etelek;
+    }
+
+    public int getOsszeg() {
+        int osszeg = 0;
+        for (int i = 0; i < etelek.size(); i++) {
+            osszeg += etelek.get(i).getAr();
+        }
+        return osszeg;
+    }
 }
